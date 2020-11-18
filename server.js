351d418ba4694add626d438dff77aa8f2f27239c
TungNguyen <17521236@gm.uit.edu.vn>
2020-11-19 03:00:40 +0700

    deploy client to heroku

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static('./dist/chatcord'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/chatcord/' }
+  );
+});
 
 // Initialize the app.
-var server = app.listen(process.env.PORT || 4200, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+app.listen(process.env.PORT || 4200, function () {
+  console.log("App now running on port");
 });
